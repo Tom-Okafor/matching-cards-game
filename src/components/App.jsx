40f8418d02800586354d9288c1cb9616355d943e
Tom Okafor <tmeka008@gmail.com>
2025-01-13 02:02:53 +0100

    feat(ui): add functionality to toggle display mode.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,21 @@
 import "../styles/App.css";
 import AllCards from "./AllCards";
 import GameRecords from "./GameRecords";
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useState } from "react";
 import { initialCardState } from "../constants";
 import { CardReducer } from "./CardReducer";
 
 const CardContext = createContext();
 function App() {
   const [state, dispatch] = useReducer(CardReducer, initialCardState);
+  const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  const [displayMode, setDisplayMode] = useState(mediaQuery.matches);
+  function changeDisplayMode() {
+    setDisplayMode((prevMode) => !prevMode);
+  }
+
   return (
-    <>
+    <div className={displayMode ? "dark" : ""}>
       <h1>Matching pairs game</h1>
       <img
         src="/assets/mode.svg"
@@ -18,12 +24,13 @@ function App() {
         aria-roledescription="This button toggles between light and dark mode"
         tabIndex={1}
         className="toggle"
+        onClick={changeDisplayMode}
       />
       <CardContext.Provider value={{ state, dispatch }}>
         <GameRecords />
         <AllCards />
       </CardContext.Provider>
-    </>
+    </div>
   );
 }
 
