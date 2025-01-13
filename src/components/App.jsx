f4a2e49f2eab0a2238194314d0fdf0d0d66ea165
Tom Okafor <tmeka008@gmail.com>
2025-01-13 01:26:11 +0100

    style(toggle button): create and style button for dark mode toggle.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,14 @@ function App() {
   return (
     <>
       <h1>Matching pairs game</h1>
-
+      <img
+        src="/assets/mode.svg"
+        alt=""
+        role="button"
+        aria-roledescription="This button toggles between light and dark mode"
+        tabIndex={1}
+        className="toggle"
+      />
       <CardContext.Provider value={{ state, dispatch }}>
         <GameRecords />
         <AllCards />
